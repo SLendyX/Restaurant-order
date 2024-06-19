aedae94525e8cbefbfba6dcb4eea9da954b343ed
SLendyX <slendyx2002@gmail.com>
2024-06-19 16:05:00 +0300

    order list added

--- index.js
+++ index.js
@@ -1,45 +1,109 @@
 import { menuArray } from "./data.js"
 
 const menuList = document.getElementById("list-element");
+const orderFooter = document.getElementById("order-footer")
+const itemList = document.getElementById("item-list")
+const totalPrice = document.getElementById("total-price")
+
+let orderArray = []
 
 function generateMenu(){
     let listItem = ``
 
     menuArray.forEach(menuItem => { 
         listItem+=`
-        <li>
+        <li class="spacing-menu">
         <div class="menu-item-container">
             <i>${menuItem.emoji}</i>
             <div>
                 <span class="title-span">${menuItem.name}</span>
-                <p>${menuItem.ingredients.join(", ")}</p>
+                <p class="ingredient-list">${menuItem.ingredients.join(", ")}</p>
                 <span class="price-span">$${menuItem.price}</span>
             </div>
         </div>
-        <button class="add-btn">+</button>
+        <button class="add-btn" data-id="${menuItem.id}">+</button>
         </li>`
     })
 
     menuList.innerHTML = listItem;
 
+    const menuButtons = document.querySelectorAll(".add-btn");
+    
+    for(let btn of menuButtons){    
+        btn.addEventListener("click" ,() => {
+            const itemObject = menuArray.filter((menuItem)=>{
+                return menuItem.id == btn.dataset.id
+            })[0]
 
+            if(orderFooter.className === "hidden")
+                orderFooter.classList.toggle("hidden")
 
-/*
-`
-<li>
-<div class="menu-item-container">
-    <i>ITEM EMOJI</i>
-    <div>
-        <span class="title-span">ITEM NAME</span>
-        <p>INGREDIENT LIST</p>
-        <span class="price-span">$PRICE</span>
-    </div>
-</div>
-<button class="add-btn">+</button>
-</li>`
-*/
+            // const childArray = [...itemList.children]
+
+            // const duplicateItem = childArray.find((child) => child.dataset.id === btn.dataset.id)
+
+            // if(!duplicateItem){
+
+            const itemContainer = document.createElement("li")
+            itemContainer.classList+="space-between"
+            itemContainer.dataset.id = btn.dataset.id;
+
+            const newItemInList = document.createElement("li")
+            newItemInList.classList+="title-span"
+            newItemInList.textContent = `${itemObject.name}`
+            
+            const removeBtn = document.createElement("button")
+            removeBtn.classList+="remove-btn"
+            removeBtn.textContent = "remove"
+
+            removeBtn.addEventListener("click", ()=>{
+
+                const removedItemPrice = Number(removeBtn.parentElement.parentElement.children[1].textContent.substring(1))
+                removeBtn.parentElement.parentElement.parentElement.removeChild(removeBtn.parentElement.parentElement)
+
+                let hasPriceBeenFound = false
+                orderArray = orderArray.filter(function(orderItem){
+                    if(orderItem == removedItemPrice && !hasPriceBeenFound){
+                        hasPriceBeenFound = !hasPriceBeenFound;
+                        return 0;
+                    }
+                    return 1
+                })
+                try{
+                    const newTotal = orderArray.reduce(
+                        (total, price) =>  total + price)
+                
+                    totalPrice.textContent = `$${newTotal}`
+                }catch(err){
+                    orderFooter.classList.toggle("hidden")
+                }
+            })
+
+            newItemInList.appendChild(removeBtn)
+            itemContainer.appendChild(newItemInList)
+
+            const newPriceInList = document.createElement("li")
+            newPriceInList.classList+="price-span"
+            newPriceInList.textContent = `$${itemObject.price}`
+
+            itemContainer.appendChild(newPriceInList)
+
+            itemList.appendChild(itemContainer)
+
+            orderArray.push(itemObject.price)
+
+            const newTotal = orderArray.reduce( 
+                (total, price) =>  total + price)
+            
+            totalPrice.textContent = `$${newTotal}`
+            // }
+        })
+    }
+
 }
 
+//<li class="price-span">$</li>
+//<li class="title-span">Pizza <button class="remove-btn">remove</button></li>
 
 
 
